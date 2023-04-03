8e006bf8c2fb9e16e5bdce9588467d1afb4e0aa1
Jack <xuwenjiejack@gmail.com>
2023-04-03 14:11:47 +0800

    remove useless log

--- core/txpool/journal.go
+++ core/txpool/journal.go
@@ -164,7 +164,7 @@ func (journal *journal) rotate(all map[common.Address]types.Transactions) error 
 		return err
 	}
 	journal.writer = sink
-	log.Info("Regenerated local transaction journal", "transactions", journaled, "accounts", len(all))
+	// log.Info("Regenerated local transaction journal", "transactions", journaled, "accounts", len(all))
 
 	return nil
 }

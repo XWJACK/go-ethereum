39b08a6de0c5f2ce5a557a0d9e0068bc684e87b3
Jack <xuwenjiejack@gmail.com>
2024-02-19 09:54:20 +0800

    Jack modified: params/version.go

--- params/version.go
+++ params/version.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	VersionMajor = 1        // Major version component of the current release
-	VersionMinor = 13       // Minor version component of the current release
-	VersionPatch = 12       // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionMajor = 1              // Major version component of the current release
+	VersionMinor = 13             // Minor version component of the current release
+	VersionPatch = 12             // Patch version component of the current release
+	VersionMeta  = "xwjack.0.0.1" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
